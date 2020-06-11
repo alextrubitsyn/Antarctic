10980e89dbd9e943f4a310703f8a8c9079186bf4
AlexTrubitsyn <9970421@gmail.com>
2020-06-11 15:51:59 +0300

    исправляет ошибки

--- build/js/main.js
+++ build/js/main.js
@@ -2,22 +2,12 @@
 
 (function () {
 
-  var noJsPage = document.querySelector('.page--nojs');
   var noJsHeader = document.querySelector('.page-header--nojs');
-  var noJsLabel = document.querySelector('.page-label--nojs');
   var pageHeader = document.querySelector('.page-header');
   var toggleButton = pageHeader.querySelector('.page-header__toggle');
 
   if (noJsHeader) {
     noJsHeader.classList.remove('page-header--nojs');
-  }
-
-  if (noJsPage) {
-    noJsPage.classList.remove('page--nojs');
-  }
-
-  if (noJsLabel) {
-    noJsLabel.classList.remove('page-label--nojs');
   }
 
   if (toggleButton) {
