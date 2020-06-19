5d9a3803ed3d6d5f5bd9ead26ecb93c6698c85b0
AlexTrubitsyn <9970421@gmail.com>
2020-06-19 19:43:32 +0300

    переводит проект на препроцессор Sass

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@
 var gulp = require('gulp');
 var plumber = require('gulp-plumber');
 var sourcemap = require('gulp-sourcemaps');
-var less = require('gulp-less');
+var sass = require('gulp-sass');
+// var less = require('gulp-less');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var server = require('browser-sync').create();
@@ -17,10 +18,10 @@ var include = require('posthtml-include');
 var del = require('del');
 
 gulp.task('css', function () {
-  return gulp.src('source/less/style.less')
+  return gulp.src('source/sass/style.scss')
       .pipe(plumber())
       .pipe(sourcemap.init())
-      .pipe(less())
+      .pipe(sass())
       .pipe(postcss([autoprefixer()]))
       .pipe(gulp.dest('build/css'))
       .pipe(csso())
@@ -29,6 +30,20 @@ gulp.task('css', function () {
       .pipe(gulp.dest('build/css'))
       .pipe(server.stream());
 });
+
+// gulp.task('css', function () {
+//   return gulp.src('source/less/style.less')
+//       .pipe(plumber())
+//       .pipe(sourcemap.init())
+//       .pipe(less())
+//       .pipe(postcss([autoprefixer()]))
+//       .pipe(gulp.dest('build/css'))
+//       .pipe(csso())
+//       .pipe(rename('style.min.css'))
+//       .pipe(sourcemap.write('.'))
+//       .pipe(gulp.dest('build/css'))
+//       .pipe(server.stream());
+// });
 
 gulp.task('server', function () {
   server.init({
@@ -41,7 +56,7 @@ gulp.task('server', function () {
     ui: false
   });
 
-  gulp.watch('source/less/**/*.less', gulp.series('css'));
+  gulp.watch('source/sass/**/*.scss', gulp.series('css'));
   gulp.watch('source/img/icon-*.svg', gulp.series('sprite', 'html', 'refresh'));
   gulp.watch('source/*.html', gulp.series('html', 'refresh'));
 });
