16b648b9d03b33a10ee918ada94677968f092d3e
AlexTrubitsyn <9970421@gmail.com>
2020-06-20 13:41:09 +0300

    исправляет ошибки

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ var gulp = require('gulp');
 var plumber = require('gulp-plumber');
 var sourcemap = require('gulp-sourcemaps');
 var sass = require('gulp-sass');
-// var less = require('gulp-less');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var server = require('browser-sync').create();
@@ -30,20 +29,6 @@ gulp.task('css', function () {
       .pipe(gulp.dest('build/css'))
       .pipe(server.stream());
 });
-
-// gulp.task('css', function () {
-//   return gulp.src('source/less/style.less')
-//       .pipe(plumber())
-//       .pipe(sourcemap.init())
-//       .pipe(less())
-//       .pipe(postcss([autoprefixer()]))
-//       .pipe(gulp.dest('build/css'))
-//       .pipe(csso())
-//       .pipe(rename('style.min.css'))
-//       .pipe(sourcemap.write('.'))
-//       .pipe(gulp.dest('build/css'))
-//       .pipe(server.stream());
-// });
 
 gulp.task('server', function () {
   server.init({
